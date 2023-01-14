741246b9645a63bfd38e7bff0d70bb52f12ad2b1
zeroClearAmerican <35619771+zeroClearAmerican@users.noreply.github.com>
2023-01-14 16:07:40 -0600

    Added additional swerve modules and added 2333 channels to RobotMap

--- src/main/java/frc/robot/config/RobotMap.java
+++ src/main/java/frc/robot/config/RobotMap.java
@@ -1,8 +1,26 @@
 package frc.robot.config;
 
 public class RobotMap {
-  public static final byte kTalon1Id = 1;
-  public static final byte kFalcon1Id = 2;
-  public static final byte kEncoder1AIOChannel = 1;
-  public static final short kEncoderBasePositionOffset = 0;
-}
+
+  // Motor CAN Id Constants
+  public static final byte TALON1_CAN_ID = 3;
+  public static final byte TALAON2_CAN_ID = 2;
+  public static final byte FALCON1_CAN_ID = 8;
+  public static final byte SPARK1_CAN_ID = 4;
+
+   //Joystick Constants
+   public static final byte JOYSTICK1_PORT_NJUMBER = 0;
+   public static final byte TALON1_AXIS = 0;
+   public static final byte TALON2_AXIS = 1;
+   public static final byte SPARK1AXIS = 2;
+   public static final byte FALCON1_AXIS = 3;
+   public static final int SOLENOID_2_ID = 1;
+   public static final int SOLENOID_1_ID = 0;
+   public static final int FALCON_2_ID = 0;
+   public static final int TALON_1_ID = 0;
+   public static byte kTalon1Id;
+public static byte kFalcon1Id;
+
+public static byte kEncoder1AIOChannel;
+public static short kEndcoderBasePositionOffset;
+} 

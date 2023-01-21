d7c132c5077d2919aa1bda90ad63441ce1537a39
rcprime31 <117952477+rcprime31@users.noreply.github.com>
2023-01-21 16:14:27 -0600

    Got 2333 robot driving, working on characterization

--- src/main/java/frc/robot/prime/drive/swerve/PrimeSwerveDriveTrain.java
+++ src/main/java/frc/robot/prime/drive/swerve/PrimeSwerveDriveTrain.java
@@ -1,29 +1,28 @@
 package frc.robot.prime.drive.swerve;
 
-// import com.kauailabs.navx.frc.AHRS;
-
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.SerialPort.Port;
 import frc.robot.config.RobotMap;
 import frc.robot.prime.models.PidConstants;
 
 public class PrimeSwerveDriveTrain {
-  public static final double kMaxSpeed = 4.572; // in meters per second, 15 feet per second
-  public static final double kMaxAngularSpeed = Math.PI; // 180 degrees per second, half rotation
+  public static final double kMaxSpeed = 0.27432; // in meters per second
+  public static final double kMaxAngularSpeed = Math.PI * 0.66; // 180 degrees per second, half rotation
 
   // Default PID values for steering each module and driving each module
   public static final PidConstants kDrivePidConstants = new PidConstants(0.01, 0, 0);
-  public static final PidConstants kSteeringPidConstants = new PidConstants(0.8, 0.00, 0);
+  public static final PidConstants kSteeringPidConstants = new PidConstants(1);
 
   // Initialize "locations" of each wheel in terms of x, y translation in meters from the origin (middle of the robot)
-  final Translation2d frontLeftLocation = new Translation2d(1, 1);  // using 1's right now for ease of testing
-  final Translation2d frontRightLocation = new Translation2d(1, -1);
-  final Translation2d rearLeftLocation = new Translation2d(-1, 1);
-  final Translation2d rearRightLocation = new Translation2d(-1, -1);
+  double halfWheelBase = RobotMap.kRobotWheelBaseMeters / 2;
+  double halfTrackWidth = RobotMap.kRobotTrackWidthMeters / 2;
+  final Translation2d frontLeftLocation =   new Translation2d(-halfTrackWidth, halfWheelBase);  // using 1's right now for ease of testing
+  final Translation2d frontRightLocation =  new Translation2d(halfTrackWidth,  halfWheelBase);
+  final Translation2d rearLeftLocation =    new Translation2d(-halfTrackWidth, -halfWheelBase);
+  final Translation2d rearRightLocation =   new Translation2d(halfTrackWidth,  -halfWheelBase);
 
   // Build serve drive modules with encoder channel & offset, and CAN IDs for drive and steering motors
   // Front Left
@@ -31,28 +30,32 @@ public class PrimeSwerveDriveTrain {
     RobotMap.kFrontLeftSteeringMotorId, 
     RobotMap.kFrontLeftDrivingMotorId, 
     RobotMap.kFrontLeftEncoderAIOChannel,
-    RobotMap.kFrontLeftEncoderOffset);
+    RobotMap.kFrontLeftEncoderOffset,
+    true);
 
-  // // Front Right
-  // final PrimeSwerveModule m_frontRightModule = new PrimeSwerveModule(
-  //   RobotMap.kFrontRightSteeringMotorId, 
-  //   RobotMap.kFrontRightDrivingMotorId, 
-  //   RobotMap.kFrontRightEncoderAIOChannel, 
-  //   RobotMap.kFrontRightEncoderOffset);
+  // Front Right
+  public final PrimeSwerveModule m_frontRightModule = new PrimeSwerveModule(
+    RobotMap.kFrontRightSteeringMotorId, 
+    RobotMap.kFrontRightDrivingMotorId, 
+    RobotMap.kFrontRightEncoderAIOChannel, 
+    RobotMap.kFrontRightEncoderOffset,
+    false);
 
   // Rear Left
-  final PrimeSwerveModule m_rearLeftModule = new PrimeSwerveModule(
+  public final PrimeSwerveModule m_rearLeftModule = new PrimeSwerveModule(
     RobotMap.kRearLeftSteeringMotorId, 
     RobotMap.kRearLeftDrivingMotorId, 
     RobotMap.kRearLeftEncoderAIOChannel, 
-    RobotMap.kRearLeftEncoderOffset);
+    RobotMap.kRearLeftEncoderOffset,
+    true);
 
-  // // Rear Right
-  // final PrimeSwerveModule m_rearRightModule = new PrimeSwerveModule(
-  //   (byte)RobotMap.kRearRightSteeringMotorId, 
-  //   (byte)RobotMap.kRearRightDrivingMotorId, 
-  //   RobotMap.kRearRightEncoderAIOChannel, 
-  //   RobotMap.kRearRightEncoderOffset);
+  // Rear Right
+  public final PrimeSwerveModule m_rearRightModule = new PrimeSwerveModule(
+    RobotMap.kRearRightSteeringMotorId, 
+    RobotMap.kRearRightDrivingMotorId, 
+    RobotMap.kRearRightEncoderAIOChannel, 
+    RobotMap.kRearRightEncoderOffset,
+    false);
 
   // Build a gyro and a kinematics class for our drive
   // final AHRS m_gyro = new AHRS(Port.kUSB);
@@ -76,12 +79,8 @@ public class PrimeSwerveDriveTrain {
     SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, kMaxSpeed);
 
     m_frontLeftModule.setDesiredState(swerveModuleStates[0]);
-    // m_frontRightModule.setDesiredState(swerveModuleStates[1]);
+    m_frontRightModule.setDesiredState(swerveModuleStates[1]);
     m_rearLeftModule.setDesiredState(swerveModuleStates[2]);
-    // m_rearRightModule.setDesiredState(swerveModuleStates[3]);
-    // m_frontLeftModule.driveSimple(fwd, rot);
-    // m_frontRightModule.driveSimple(fwd, rot);
-    // m_rearLeftModule.driveSimple(fwd, rot);
-    // m_rearRightModule.driveSimple(fwd, rot);
+    m_rearRightModule.setDesiredState(swerveModuleStates[3]);
   }
 }

7029e361343246911fb04e1c4cb708d5fdc7b692
jerry132435 <115999634+DjAce626@users.noreply.github.com>
2023-02-03 17:36:45 -0600

    PrimeSwerveModule subsystems

--- src/main/java/frc/robot/subsystems/PrimeSwerveModuleSubsystem.java
+++ src/main/java/frc/robot/subsystems/PrimeSwerveModuleSubsystem.java
@@ -1,17 +1,13 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.prime.drive.swerve.PrimeSwerveDriveTrain;
-import frc.robot.prime.drive.swerve.PrimeSwerveModule;
 import frc.robot.sensors.MA3Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -38,6 +34,7 @@ public class PrimeSwerveModuleSubsystem  extends SubsystemBase{
       PrimeSwerveDriveTrain.kSteeringPidConstants.kD
     );
  }
+
  public void setDesiredState(SwerveModuleState desiredState){
     var encoderRotation = m_encoder.getRotation2d().rotateBy(new Rotation2d(90));
     desiredState = SwerveModuleState.optimize(desiredState, encoderRotation);
@@ -48,14 +45,10 @@ public class PrimeSwerveModuleSubsystem  extends SubsystemBase{
 
     var steeringOutput = m_steeringPIDController.calculate(currentPositionRadians, desiredPositionRadians);
     steeringOutput = MathUtil.applyDeadband(steeringOutput, 0.1);
-    
-    
  }
 
  public void driveSimple(double fwd, double rot){
     m_driveMotor.set(fwd);
     m_steeringMotor.set(rot);
  }
-    
-    
 }
